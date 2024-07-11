0969607988bda0135762ff7ee9d199b8e19d0375
devclaudiocassimiro <claudio.silva@jala.university>
2024-07-11 20:54:21 -0300

    feat: implements component Flag

---
+++ src/components/Flag/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+type FlagProps = {
+  children: ReactNode;
+};
+
+export default function Flag({ children }: FlagProps) {
+  return (
+    <div className="w-full max-w-96 h-8 flex gap-1 items-center bg-flag px-2">
+      <p className="max-[463px]:text-xs text-white font-semibold text-base leading-5">
+        {children}
+      </p>
+    </div>
+  );
+}
