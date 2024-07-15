7c963b21449e889b3a065462a8c1912ce62c1109
devclaudiocassimiro <claudio.silva@jala.university>
2024-07-14 21:51:20 -0300

    feat: implements component PaymentConfirmation

---
+++ src/app/payment-confirmation/page.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import PaymentConfirmationForm from "@/components/PaymentConfirmationForm";
+import PixInfo from "@/components/PixInfo/Index";
+
+export default function PaymentConfirmation() {
+  return (
+    <main className="flex flex-col items-center min-h-screen p-4">
+      <Header
+        title="João, pague o restante em 1x no cartão"
+        className="w-[422px]"
+      />
+      <PaymentConfirmationForm />
+      <PixInfo page="payment-confirmation" />
+      <Footer />
+    </main>
+  );
+}
