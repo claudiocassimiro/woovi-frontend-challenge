63f0a17d4a1c89f7a3eb6ef6ee07adceedba065c
devclaudiocassimiro <claudio.silva@jala.university>
2024-07-14 21:53:40 -0300

    fix: change from p to span

--- src/components/Flag/index.tsx
+++ src/components/Flag/index.tsx
@@ -7,9 +7,9 @@ type FlagProps = {
 export default function Flag({ children }: FlagProps) {
   return (
     <div className="w-full max-w-96 h-8 flex gap-1 items-center bg-flag px-2">
-      <p className="max-[463px]:text-xs text-white font-semibold text-base leading-5">
+      <span className="max-[463px]:text-xs text-white font-semibold text-base leading-5">
         {children}
-      </p>
+      </span>
     </div>
   );
 }
